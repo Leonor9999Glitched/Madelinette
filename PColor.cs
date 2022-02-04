6e2e74c7ee7548055622169de9f3fb5aadeb2a85
Leonor9999Glitched <58555862+Leonor9999Glitched@users.noreply.github.com>
2022-02-04 14:50:37 +0000

    Add Class PColor to give color to the pieces

---
+++ PColor.cs
@@ -0,0 +1,12 @@
+namespace Madelinette
+{
+    ///<summary>Cor das peças</summary>
+    public enum PColor
+    {
+        ///<summary>Cor branca</summary>
+        White,
+
+        ///<summary>Cor preta</summary>
+        Black
+    }
+}
--- Peca.cs
+++ Peca.cs
@@ -4,5 +4,22 @@ namespace Madelinette.Common
 {
     public class Peca
     {
+        /// <summary>
+        /// Cor da peça
+        /// </summary>
+        private PColor color;
+
+        public Peca(PColor color)
+        {
+            this.color = color;
+        }
+
+        public bool IsColor(PColor color) => this.color;
+
+        public override string ToString()
+        {
+            return $"{color}";
+        }
     }
+
 }
